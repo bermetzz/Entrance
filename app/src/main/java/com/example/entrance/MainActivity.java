290ee7091770af5e56ae996b9947a2cbcf0f5bf6
bermetzz <zulfibaevab@gmail.com>
2022-11-24 10:31:39 +0600

    corrected

--- app/src/main/java/com/example/entrance/MainActivity.java
+++ app/src/main/java/com/example/entrance/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.entrance;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.Spannable;
-import android.text.SpannableStringBuilder;
-import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.text.style.ForegroundColorSpan;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -29,18 +23,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        );
         setContentView(R.layout.activity_main);
-//        addListenerOnInputs();
         addListenerOnButton();
         login.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
@@ -74,14 +69,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-//    public void addListenerOnInputs() {
-//        password = (EditText)findViewById(R.id.password);
-//        login = (EditText)findViewById(R.id.email);
-//        btn = (Button)findViewById(R.id.done);
-//        if(!TextUtils.isEmpty(login.getText().toString()) || !TextUtils.isEmpty(password.getText().toString())){
-//            btn.setBackgroundResource(R.color.orange);                    }
-//        else btn.setBackgroundResource(R.color.gray);
-//    }
+
 
 
     public void addListenerOnButton() {
